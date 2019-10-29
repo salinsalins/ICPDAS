004d7de54ee0f1a1d0de0dbceed8196f06555114
Sanin <sanin_andrey@mail.ru>
2019-10-29 17:51:46 +0700

    commit

--- ModbusClientPolling.py
+++ ModbusClientPolling.py
@@ -9,12 +9,13 @@ import time
 from threading import Thread, Lock
 from pyModbusTCP.client import ModbusClient
 
-SERVER_HOST = "192.168.0.47"
+SERVER_HOST = "192.168.1.108"
 SERVER_PORT = 502
 
 # set global
 regs = []
 t = time.time()
+n = 0
 
 # init a thread lock
 regs_lock = Lock()
@@ -23,6 +24,8 @@ regs_lock = Lock()
 # modbus polling thread
 def polling_thread():
     global regs
+    global t
+    global n
     c = ModbusClient(host=SERVER_HOST, port=SERVER_PORT)
     # polling loop
     while True:
@@ -30,14 +33,15 @@ def polling_thread():
         if not c.is_open():
             c.open()
         # do modbus reading on socket
-        reg_list = c.read_input_registers(0, 10)
+        reg_list = c.read_holding_registers(0, 10)
         # if read is ok, store result in regs (with thread lock synchronization)
         if reg_list:
             with regs_lock:
+                t = time.time()
+                n += 1
                 regs = list(reg_list)
-            t = time.time()
         # 1s before next polling
-        #time.sleep(1)
+        time.sleep(0.01)
 
 
 # start polling thread
@@ -46,14 +50,17 @@ tp = Thread(target=polling_thread)
 tp.daemon = True
 tp.start()
 
+t0 = time.time()
+
 # display loop (in main thread)
 while True:
     # print regs list (with thread lock synchronization)
     with regs_lock:
-        print(time.clock(),' ', end='')
+        t1 = time.time()
+        print('%13.2f ' % t, end='')
         for r in regs:
             print(hex(r) + ' ', end='')
-        print(' ')
+        print(' %d' % (n/(t1-t0+1e-15)))
         #print(regs)
     # 1s before next print
-    time.sleep(0.01)
+    time.sleep(0.1)
