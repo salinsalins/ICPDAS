b417a39c30e70c613a1d923ef4e82ba7b2957331
Sanin <sanin_andrey@mail.ru>
2019-11-05 10:30:44 +0700

    commit

--- ET7000.py
+++ ET7000.py
@@ -4,39 +4,39 @@ from pyModbusTCP.client import ModbusClient
 class ET7000:
     AI_ranges = {
         0x00: {
-            'units': 'V',
             'min': -0.015,
-            'max': 0.015
+            'max': 0.015,
+            'units': 'V'
         },
         0x01: {
-            'units': 'V',
             'min': -0.05,
-            'max': 0.05
+            'max': 0.05,
+            'units': 'V'
         },
         0x02: {
-            'units': 'V',
             'min': -0.1,
-            'max': 0.1
+            'max': 0.1,
+            'units': 'V'
         },
         0x03: {
-            'units': 'V',
             'min': -0.5,
-            'max': 0.5
+            'max': 0.5,
+            'units': 'V'
         },
         0x04: {
-            'units': 'V',
             'min': -1.,
-            'max': 1.
+            'max': 1.,
+            'units': 'V'
         },
         0x05: {
-            'units': 'V',
             'min': -2.5,
-            'max': 2.5
+            'max': 2.5,
+            'units': 'V'
         },
         0x06: {
-            'units': 'A',
             'min': -20.0e-3,
-            'max': 20.0e-3
+            'max': 20.0e-3,
+            'units': 'A'
         },
         0x07: {
             'units': 'A',
@@ -84,21 +84,26 @@ class ET7000:
     }
 
     class AI:
-        def __init__(self, _addr, _min, _max, convert=None):
+        def __init__(self, _addr, _min=-10.0, _max=10.0, _units='V'):
             self.addr = _addr  # номер канала АЦП
-            self.min = _min  # минимальное значение в вольтах
+            # if second argument is dictionary of type range
+            try:
+                self.min = _min['min']
+                self.max = _min['max']
+                self.units = _min['units']
+                return
+            except:
+                pass
+            self.min = _min  # минимальное значение
             self.max = _max  # максимальное
-            if self.convert is None:
-                self.convert = self.AI()
-            else:
-                self.convert = convert
+            self.units = _units
 
-        # default conversion from int to volts
-        def toV(self, b):
-            # обрабатывается вусего 2 случая - минимум нулевой
+        # default conversion from quanta to real units
+        def convert(self, b):
+            # обрабатывается 2 случая - минимум нулевой
             if self.min == 0 and self.max > 0:
                 return self.max * b / 0xffff
-                # и минимум по модулю равен максимуму
+            # и минимум по модулю равен максимуму
             if self.min == -self.max and self.max > 0:
                 one = 0xffff / 2
                 if b <= one:
@@ -112,12 +117,21 @@ class ET7000:
         self._host = host
         self._port = port
         self._client = ModbusClient(host=self._host, port=self._port, auto_open=True, auto_close=True, timeout=timeout)
-        self.client.open()
+        self._client.open()
+        self._name = ''
+        self.AI_n = 0
+        self.AI_ranges = []
+        self.AI_masks = []
+        self.channels = []
+        self.init()
+
+    def init(self):
         self._name = self.read_module_name()
         if self._name not in ET7000.devices:
             print('Device %s is not supported' % hex(self._name))
-            return
-        self.n_AI = self.read_n_AI()
+        self.AI_n = self.read_AI_n()
+        self.AI_ranges = self.read_AI_range()
+        self.AI_masks = self.read_AI_mask()
 
     def read_module_name(self):
         regs = self._client.read_holding_registers(559, 1)
@@ -125,21 +139,25 @@ class ET7000:
             return regs[0]
         return None
 
-    def read_n_AI(self):
+    def read_AI_n(self):
         regs = self._client.read_input_registers(320, 1)
         if regs:
             return regs[0]
-        return None
+        return 0
 
     def read_AI_range(self):
-        regs = self._client.read_holding_registers(427, self.n_chan)
-        coils = self._client.read_coils(595, self.n_chan)
-        n = 0
-        for c in coils:
-            if c == 0:
-                regs[n] = None
-            n += 1
+        regs = self._client.read_holding_registers(427, self.AI_n)
         return regs
+
+    def read_AI_mask(self):
+        coils = self._client.read_coils(595, self.AI_n)
+        return coils
+
+    def read_AI(self):
+        regs = self._client.read_input_registers(0, self.AI_n)
+        return regs
+
+
 
     def read(self):
         pass
