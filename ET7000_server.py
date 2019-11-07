aaa9e268b2db298d409df4f15f1fb3243df3f33e
Sanin <sanin_andrey@mail.ru>
2019-11-07 18:09:39 +0700

    commit

--- ET7000_server.py
+++ ET7000_server.py
@@ -8,6 +8,7 @@ import sys
 import time
 import numpy
 
+import tango
 from tango import AttrQuality, AttrWriteType, DispLevel, DevState, DebugIt
 from tango.server import Device, attribute, command, pipe, device_property
 
@@ -16,15 +17,31 @@ from ET7000 import ET7000
 
 class ET7000_Server(Device):
 
+    type = attribute(label="type", dtype=int,
+                        display_level=DispLevel.OPERATOR,
+                        access=AttrWriteType.READ,
+                        unit="", format="%d",
+                        doc="ET7000 device type")
+
+    def read_type(self):
+        return self.name
+
     def init_device(self):
         Device.init_device(self)
+        self.et = ET7000('192.168.1.122')
+        self.name = self.et._name
+        print(hex(self.et._name))
+        db = tango.Database()
+        di = db.get_device_info('et7000_server/test/1')
+        print(di)
+
         self.__current = 0.0
         self.set_state(DevState.STANDBY)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python ET7000_server.py device_name ip_address")
-        exit(-1)
+    #if len(sys.argv) < 3:
+        #print("Usage: python ET7000_server.py device_name ip_address")
+        #exit(-1)
 
     ET7000_Server.run_server()
