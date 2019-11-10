ddba4b2cac9c3c125f56143bcc7d32804861e001
Andrey Sanin <sanin_andrey@mail.ru>
2019-11-10 10:27:29 +0700

    commit

--- ET7000_server.py
+++ ET7000_server.py
@@ -115,9 +115,9 @@ class ET7000_Server(Device):
             a = da.get_attr_by_ind(k)
             an = a.get_name()
             print(an)
-            #if an[:2] == 'ai' or an[:2] == 'ao' or an[:2] == 'di' or an[:2] == 'do':
-                #self.remove_attribute(an)
-                #print('removed ', an)
+            if an[:2] == 'ai' or an[:2] == 'ao' or an[:2] == 'di' or an[:2] == 'do':
+                print('removing ', an)
+                self.remove_attribute(an)
         cl = self.get_device_class()
         print(cl)
         am = self.dyn_att_added_methods
