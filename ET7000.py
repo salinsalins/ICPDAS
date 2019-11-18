a45ea9343bff70b3fdbe1b8b1d80d3b4297cc99b
Sanin <sanin_andrey@mail.ru>
2019-11-18 19:12:34 +0700

    commit

--- ET7000.py
+++ ET7000.py
@@ -257,7 +257,7 @@ class ET7000:
             'units': 'A'
         },
         0x32: {
-            'min': 0.0,
+            'min': -10.0,
             'max': 10.0,
             'units': 'V'
         },
@@ -267,7 +267,7 @@ class ET7000:
             'units': 'V'
         },
         0x34: {
-            'min': 0.0,
+            'min': -5.0,
             'max': 5.0,
             'units': 'V'
         },
@@ -301,33 +301,35 @@ class ET7000:
     # default conversion from quanta to real units
     @staticmethod
     def convert(b, amin, amax):
-        # обрабатывается 2 случая - минимум нулевой
-        if amin == 0 and amax > 0:
-            return amax * b / 0xffff
-        # и минимум по модулю равен максимуму
-        if amin == -amax and amax > 0:
-            one = 0xffff / 2
-            if b <= one:
-                return amax * b / one
+        b = float(b)
+        # обрабатывается 2 случая - минимум нулевой или больше 0
+        if amin >= 0 and amax > 0:
+            return amin + (amax - amin) * b / 0xffff
+        # и минимум  и максимум разного знака
+        if amin < 0 and amax > 0:
+            range = max(-amin, amax)
+            if b <= 0x7fff:
+                return range * b / 0x7fff
             else:
-                return -amax * (0xffff - b) / one
+                return range * (0x8000 - b) / 0x8000
         # в других случаях ошибка
-        return amin + (amax-amin)*b/0xffff
+        return float('nan')
 
     @staticmethod
-    def convert_to_raw(f, amin, amax):
-        # обрабатывается 2 случая - минимум нулевой
-        if amin == 0 and amax > 0:
-            return int(f * 0xffff / amax)
-        # и минимум по модулю равен максимуму
-        if amin == -amax and amax > 0:
-            one = 0xffff / 2
-            if f > 0.0:
-                return int(f * one / amax)
+    def convert_to_raw(v, amin, amax):
+        v = float(v)
+        # обрабатывается 2 случая - минимум нулевой или больше 0
+        if amin >= 0 and amax > 0:
+            return int((v - amin) / (amax - amin) * 0xffff)
+        # и минимум  и максимум разного знака
+        if amin < 0 and amax > 0:
+            if v >= 0.0:
+                return int(v * 0x7fff / amax)
             else:
-                return int(0xffff + (f * one / amax))
+                return int(0x8000 - v / amax * 0x7fff)
         # в других случаях ошибка
-        return 0xffff
+        return 0
+
 
     def __init__(self, host, port=502, timeout=0.15):
         self._host = host
