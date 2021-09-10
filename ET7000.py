f4b452cef6689c505aaec3b275f88b4143472a06
salinsalins <sanin_andrey@mail.ru>
2021-09-10 10:25:13 +0700

    commit

--- ET7000.py
+++ ET7000.py
@@ -5,6 +5,7 @@ from pyModbusTCP.client import ModbusClient
 
 NaN = float('nan')
 
+
 class ET7000:
     ranges = {
         0x00: {
@@ -692,6 +693,326 @@ class ET7000:
         if result:
             return result
         return False
+
+
+class FakeET7000(ET7000):
+
+    class _client:
+        def __init__(self, host, port=502, timeout=0.15, logger=None):
+            self.count = 0
+            self.data = {
+                320: 6,
+                595: 1,
+                427: 4,
+            }
+
+        def read_holding_registers(self, n, m):
+            return [self.data[n]] * m
+
+        def read_input_registers(self, n, m):
+            # regs = self.client.read_input_registers(320, 1)
+            # regs = [6]
+            if n in self.data:
+                return [self.data[n]] * m
+            # regs = self.client.read_input_registers(0+channel, n)
+            self.count += 1
+            if self.count == 0x7fff:
+                self.count = 0
+            regs = [self.count for i in range(m)]
+            return regs
+
+        def read_coils(self, n, m):
+            return self.data[n] * m
+
+        def auto_close(self, x):
+            return x
+
+    def __init__(self, host, port=502, timeout=0.15, logger=None):
+        self.count = [0, 0x7fff/6, 0x7fff/6*2, 0x7fff/6*3, 0x7fff/6*4, 0x7fff/6*5]
+        # logger confid
+        if logger is None:
+            logger = logging.getLogger(__name__)
+        self.logger = logger
+        # default device type_str
+        self._name = 0
+        self.type = '0000'
+        # default ai
+        self.AI_n = 0
+        self.AI_masks = []
+        self.AI_ranges = []
+        self.AI_min = []
+        self.AI_max = []
+        self.AI_units = []
+        self.AI_raw = []
+        self.AI_values = []
+        # default ao
+        self.AO_n = 0
+        self.AO_ranges = []
+        self.AO_min = []
+        self.AO_max = []
+        self.AO_units = []
+        self.AO_raw = []
+        self.AO_values = []
+        self.AO_write_raw = []
+        self.AO_write_values = []
+        self.AO_write_result = False
+        # default di
+        self.DI_n = 0
+        self.DI_values = []
+        # default do
+        self.DO_n = 0
+        self.DO_values = []
+        # modbus client
+        #self.client = ModbusClient(host, port, auto_open=True, auto_close=True, timeout=timeout)
+        self._client = empty()
+        self._client.auto_close = lambda x: x
+        #status = self.client.open()
+        status = True
+        if not status:
+            #print('ET7000 device at %s is offline' % host)
+            self.logger.error('ET7000 device at %s is offline' % host)
+            return
+        # read module type
+        self._name = self.read_module_name()
+        self.type = hex(self._name).replace('0x', '')
+        if self._name not in ET7000.devices:
+            #print('ET7000 device type_str %s probably not supported' % hex(self.type))
+            self.logger.warning('ET7000 device type_str %s probably not supported' % hex(self._name))
+        # ai
+        self.AI_n = self.read_AI_n()
+        self.AI_masks = [False] * self.AI_n
+        self.AI_ranges = [0xff] * self.AI_n
+        self.AI_raw = [0] * self.AI_n
+        self.AI_values = [float('nan')] * self.AI_n
+        self.AI_units = [''] * self.AI_n
+        self.read_AI_masks()
+        self.read_AI_ranges()
+        self.AI_convert = [lambda x: x] * self.AI_n
+        for n in range(self.AI_n):
+            r = self.AI_ranges[n]
+            self.AI_units[n] = ET7000.ranges[r]['units']
+            self.AI_convert[n] = ET7000.ai_convert_function(r)
+        # ao
+        self.AO_n = self.read_AO_n()
+        self.AO_ranges = [0xff] * self.AO_n
+        self.AO_raw = [0] * self.AO_n
+        self.AO_values = [float('nan')] * self.AO_n
+        self.AO_write_values = [float('nan')] * self.AO_n
+        self.AO_units = [''] * self.AO_n
+        self.AO_write = [0] * self.AO_n
+        self.AO_write_raw = [0] * self.AO_n
+        self.read_AO_ranges()
+        self.AO_convert = [lambda x: x] * self.AI_n
+        self.AO_convert_write = [lambda x: 0] * self.AI_n
+        for n in range(self.AO_n):
+            r = self.AO_ranges[n]
+            self.AO_units[n] = ET7000.ranges[r]['units']
+            self.AO_convert[n] = ET7000.ai_convert_function(r) # !!! ai_convert for reading
+            self.AO_convert_write[n] = ET7000.ao_convert_function(r)  # !!! ao_convert for writing
+        # di
+        self.DI_n = self.read_DI_n()
+        self.DI_values = [False] * self.DI_n
+        # do
+        self.DO_n = self.read_DO_n()
+        self.DO_values = [False] * self.DO_n
+        self.DO_write = [False] * self.DO_n
+
+    def read_module_name(self):
+        return 0x7026
+
+    # AI functions
+    def ai_read_n(self):
+        return 6
+
+    def ai_read_masks(self):
+        return [True] * 6
+
+    def ai_read_ranges(self):
+        return [0x04] * 6
+
+ ##############
+    def ai_read(self, channel=None):
+        if channel is None:
+            n = self.AI_n
+            channel = 0
+        else:
+            n = 1
+        #regs = self.client.read_input_registers(0+channel, n)
+        self.count[0] += 1
+        if self.count[0] == 0x7fff:
+            self.count[0] = 0
+        regs = [self.count[0] for i in range(n)]
+        if regs and len(regs) == n:
+            self.AI_raw[channel:channel+n] = regs
+        if n == 1:
+            return regs[0]
+        return regs
+
+    def ai_read_channel(self, k:int):
+        v = float('nan')
+        if self.AI_masks[k]:
+            #regs = self.client.read_input_registers(0+k, 1)
+            self.count[k] += 1
+            if self.count[k] == 0x7fff:
+                self.count[k] = 0
+            regs = [self.count[k]]
+            if regs:
+                self.AI_raw[k] = regs[0]
+                v = self.AI_convert[k](regs[0])
+        self.AI_values[k] = v
+        return v
+
+    # AO functions
+    def read_AO_n(self):
+        #regs = self.client.read_input_registers(330, 1)
+        regs = [2]
+        if regs and regs[0] != 0:
+            return regs[0]
+        #regs = self.client.read_input_registers(130, 1)
+        #if regs:
+        #    return regs[0]
+        return 0
+
+    def read_AO_ranges(self):
+        #regs = self.client.read_holding_registers(459, self.ao_n)
+        regs = [0x33,0x33]
+        if regs and len(regs) == self.AO_n:
+            self.AO_ranges = regs
+        return regs
+
+    def read_AO_raw(self, channel=None):
+        if channel is None:
+            n = self.AO_n
+            channel = 0
+        else:
+            n = 1
+        #regs = self.client.read_holding_registers(0+channel, n)
+        regs = [0,0]
+        if regs and len(regs) == n:
+            self.AO_raw[channel:channel+n] = regs
+        if n == 1:
+            return regs[0]
+        return regs
+
+    def write_AO_raw(self, regs):
+        #result = self.client.write_multiple_registers(0, regs)
+        result = True
+        self.AO_write_result = result
+        if len(regs) == self.AO_n:
+            self.AO_write_raw = regs
+        return result
+
+    def convert_AO(self):
+        raw = self.AO_raw
+        for k in range(self.AO_n):
+            self.AO_values[k] = self.AO_convert[k](raw[k])
+        return self.AO_values
+
+    def convert_to_raw_AO(self, values=None):
+        if values is None:
+            values = self.AO_write_values
+        answer = []
+        for k in range(len(values)):
+            answer.append(self.AO_convert_write[k](values[k]))
+        return answer
+
+    def read_AO(self):
+        self.AO_raw = self.read_AO_raw()
+        self.convert_AO()
+        return self.AO_values
+
+    def read_AO_channel(self, k: int):
+        v = float('nan')
+        #regs = self.client.read_holding_registers(0+k, 1)
+        regs = [0]
+        if regs:
+            v = self.AO_convert[k](regs[0])
+            self.AO_values[k] = v
+        return v
+
+    def write_AO(self, values):
+        self.AO_write_values = values
+        regs = ET7000.convert_to_raw_AO(values)
+        result = self.write_AO_raw(regs)
+        return result
+
+    def write_AO_channel(self, k: int, value):
+        raw = self.AO_convert_write[k](value)
+        #result = self.client.write_single_register(0+k, raw)
+        result = True
+        self.AO_write_result = result
+        if result:
+            self.AO_write_values[k] = value
+            self.AO_write_raw[k] = raw
+            pass
+        return result
+
+    # DI functions
+    def read_DI_n(self):
+        #regs = self.client.read_input_registers(300, 1)
+        regs = [2]
+        if regs and regs[0] != 0:
+            return regs[0]
+        #regs = self.client.read_input_registers(100, 1)
+        #if regs:
+        #    return regs[0]
+        return 0
+
+    def read_DI(self):
+        #regs = self.client.read_discrete_inputs(0, self.di_n)
+        regs = [1 for i in range(self.DI_n)]
+        if regs:
+            self.DI_values = regs
+        return self.DI_values
+
+    def read_DI_channel(self, k: int):
+        #reg = self.client.read_discrete_inputs(0+k, 1)
+        reg = [1]
+        if reg:
+            self.DI_values[k] = reg[0]
+            return reg[0]
+        return None
+
+    # DO functions
+    def read_DO_n(self):
+        self.DO_time = time.time()
+        #regs = self.client.read_input_registers(310, 1)
+        regs = [2]
+        if regs and regs[0] != 0:
+            return regs[0]
+        #regs = self.client.read_input_registers(110, 1)
+        #if regs:
+        #    return regs[0]
+        return 0
+
+    def read_DO(self):
+        #regs = self.client.read_coils(0, self.di_n)
+        regs = [1,1]
+        if regs:
+            self.DI_values = regs
+        return self.DI_values
+
+    def read_DO_channel(self, k: int):
+        #reg = self.client.read_coils(0+k, 1)
+        reg = [1]
+        if reg:
+            self.DO_values[k] = reg[0]
+            return reg[0]
+        return None
+
+    def write_DO(self, values):
+        self.DO_write = values
+        #self.DO_write_result = self.client.write_multiple_coils(0, values)
+        self.DO_write_result = True
+        return self.DO_write_result
+
+    def write_DO_channel(self, k: int, value: bool):
+        #result = self.client.write_single_coil(0+k, value)
+        result = True
+        self.DO_write_result = result
+        if result:
+            self.DO_write[k] = value
+        return result
 
 
 if __name__ == "__main__":
